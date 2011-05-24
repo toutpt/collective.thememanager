15a5129ba6cbf15e8d550fdfc775b7ae770872ef
JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
2011-05-24 18:04:28 +0200

    update install

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(name='collective.thememanager',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'plone.app.theming',
+          'plone.app.dexterity',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
